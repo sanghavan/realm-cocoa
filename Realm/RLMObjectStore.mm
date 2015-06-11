e242dbf13401c3ccda9bfb40b7be34012a104db4
Thomas Goyne <tg@realm.io>
2015-06-11 13:14:22 -0700

    Validate that the value for an array property is enumerable even if validateNested is false
    Not doing this resulted in an unrecognized selector exception being thrown (and
    NSLogged) rather than our invalid property value exception being thrown.

--- Realm/RLMObjectStore.mm
+++ Realm/RLMObjectStore.mm
@@ -541,11 +541,11 @@ static void RLMValidateValueForProperty(__unsafe_unretained id const obj,
             }
             break;
         case RLMPropertyTypeArray: {
-            if (validateNested) {
-                if (obj != nil && obj != NSNull.null) {
-                    if (![obj conformsToProtocol:@protocol(NSFastEnumeration)]) {
-                        @throw  RLMException(@"Array property value is not enumerable.");
-                    }
+            if (obj != nil && obj != NSNull.null) {
+                if (![obj conformsToProtocol:@protocol(NSFastEnumeration)]) {
+                    @throw RLMException(@"Array property value is not enumerable.");
+                }
+                if (validateNested) {
                     id<NSFastEnumeration> array = obj;
                     for (id el in array) {
                         RLMValidateNestedObject(el, prop.objectClassName, schema, validateNested, allowMissing);
